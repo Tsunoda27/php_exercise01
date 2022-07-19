6da2b0666d96d4695f4992ec17305bfc89dac54e
Tsunoda27 <kyoiku.chikara@gmail.com>
2022-07-19 16:04:13 +0900

    制御構文1の課題_05修正中一時保存

--- 04/05_if.php
+++ 04/05_if.php
@@ -4,17 +4,17 @@ echo '$numの値を入力して下さい: ';
 $num = trim(fgets(STDIN));
 
 if ($num == 1) {
-    echo "${num}は素数ではありません";
+    $msg = '素数ではありません';
 } elseif ($num == 2) {
-    echo "${num}は素数です";
+    $msg = '素数です';
 } else {
     for ($i = 2; $i < $num; $i++) {
         if ($num % $i == 0) {
-            echo "${num}は素数ではありません";
+            $msg = '素数ではありません';
             break;
         } else {
-            echo "${num}は素数です";
-            break;
+            $msg = '素数です';
         }
     }
 }
+echo $msg;
