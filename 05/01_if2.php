a9f207651016e98630abf86eab508bd61efdcc4b
Tsunoda27 <kyoiku.chikara@gmail.com>
2022-07-19 20:20:09 +0900

    制御構文2の課題

---
+++ 05/01_if2.php
@@ -0,0 +1,17 @@
+<?php
+
+$poket_money = 1000;
+$fund_raising = 101;
+
+echo "あなたの所持金は${poket_money}です。" . PHP_EOL;
+echo "${fund_raising}円募金しました。" . PHP_EOL;
+
+while ($fund_raising <= $poket_money) {
+    $poket_money =  $poket_money - $fund_raising;
+    echo "残り残高は${poket_money}円です。" . PHP_EOL;
+    if ($fund_raising > $poket_money) {
+        break;
+    }
+    echo "${fund_raising}円募金しました。" . PHP_EOL;
+}
+echo 'あなたはこれ以上募金できません。';
---
+++ 05/03_if2.php
@@ -0,0 +1,15 @@
+<?php
+
+$foods = [
+    '朝食' => 'パン',
+    '昼食' => 'うどん',
+    '夕食' => 'ライス'
+];
+
+echo '私は、' . PHP_EOL;
+
+foreach ($foods as $time => $food) {
+    echo "${time}に${food}" . PHP_EOL;
+}
+
+echo 'を食べます';
